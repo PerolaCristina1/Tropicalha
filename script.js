feaf7cf68c17bffbba8dac9118cd85e0870c016f
PerolaCristina1 <franca.perola@escola.pr.gov.br>
2025-05-29 10:48:58 -0300

    29/53

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener "DOMContentLoaded", function () {
+    const botaoDeAcessibilidade = document.getElementById ("botao-acessibilidade")
+    const opcaoDeAcessibilidade = document.getElementById ("opcoes-acessibilidade")
+
+    botaoDeAcessibilidade.addEventListener("click" , function () {
+        
+    }
+
+}
