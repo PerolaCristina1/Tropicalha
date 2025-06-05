17747d24e6a8190305bfa1a8f286a0301c40e576
PerolaCristina1 <franca.perola@escola.pr.gov.br>
2025-06-05 09:46:30 -0300

    4/6

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ document.addEventListener "DOMContentLoaded", function () {
         opcaoDeAcessibilidade.classList.toggle(spresena-lista)
     })
 
-}
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alternar-contraste')
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+    })
+
+     alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+})
